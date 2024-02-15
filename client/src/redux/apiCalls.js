069901b01fab1a089e34fe73a74e23535046c454
shovonroy173 <shovonroy2003@gmail.com>
2024-02-15 17:46:47 +0530

    render added

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -17,7 +17,7 @@ export const register = async(dispatch , user)=>{
 export const login = async(dispatch , user)=>{
     dispatch(loginStart());
     try {
-        const res = await axios.post("https://booking-media-backend.onrender.com/api/auth/login" , user);
+        const res = await axios.post("https://booking-media-api.onrender.com/api/auth/login" , user);
         dispatch(loginSuccess(res.data));
     } catch (error) {
         dispatch(loginFailure());
